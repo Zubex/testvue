da22fe75f1d206a99203d5b6d7075d3acbc85d73
Zubex <zuspex@gmail.com>
2019-12-08 22:25:28 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let userNames = ["First", "Second", "Third"]
+let users = userNames.map( (user, index) => {
+    return {
+        'name' : user,
+        'sirname': user + " the Great " + (index + 1),
+        'userPic' : (index === 0) ? user + "face.png" : "default.png",
+    };
+} );
+
+let vm = new Vue({
+    el: "[data-app-root]",
+    data : function() {
+        return {
+            title : "Vue app",
+            users : users,
+            visible : true,
+
+
+        };
+    },
+    methods : {
+        usersToConsole : function(){
+            return console.log(this.users);
+        },
+        typeOfAction : function () {
+            return (this.visible) ? "Скрыть" : "Показать";
+        }
+    },
+    computed : {
+        usersCount : function () {
+            return this.users.length;
+        }
+    }
+
+});
+console.log('hey!');
